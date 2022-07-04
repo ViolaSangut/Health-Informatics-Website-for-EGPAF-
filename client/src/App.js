b32a4ebe63f72a44f74c0492e4b4651254ee8294
NabWW <63785037+nabww@users.noreply.github.com>
2022-07-04 15:20:56 +0300

    error depraction on client app.js

--- client/src/App.js
+++ client/src/App.js
@@ -25,11 +25,8 @@ import AddSimcards from "./Components/Pages/Inventory/AddSimcard";
 import Simcards from "./Components/Pages/Inventory/Simcards";
 import CheckAuth from "./Components/context/CheckAuth";
 import Unauthorized from "./Components/Common/Unauthorized";
-<<<<<<< HEAD
 import InventoryMain from "./Components/Pages/Inventory/InventoryMain";
-=======
 import PersistLogin from "./Components/hooks/PersistLogin";
->>>>>>> b992e2f3f2b1821702f770c340815a16be843acd
 
 const SidebarLayout = () => (
   <>
@@ -44,94 +41,50 @@ function App() {
       <>
         <ToastContainer position="top-center" />
         <Routes>
-<<<<<<< HEAD
-          <Route element={<SidebarLayout />}>
-            {/* Home */}
-            <Route element={<CheckAuth allowedRoles={[1, 2, 3, 4]} />}>
-              <Route path="/home" element={<Home />} />
-            </Route>
-
-            {/* User Management - accessed by only Admin and Super_User */}
-            <Route element={<CheckAuth allowedRoles={[3, 4]} />}>
-              <Route path="/list-user" element={<Users />} />
-              <Route path="/edit-user/:id" element={<RegisterComponent />} />
-            </Route>
-
-            {/* Facilities */}
-            <Route element={<CheckAuth allowedRoles={[1, 2, 3, 4]} />}>
-              <Route path="/facilities" element={<Facilities />} />
-            </Route>
-
-            {/* Tickets */}
-            <Route element={<CheckAuth allowedRoles={[1, 2, 3, 4]} />}>
-              <Route path="/tickets" element={<TicketsDashboard />} />
-              <Route path="/addticket" element={<AddTicketComponent />} />
-              <Route path="/edit-ticket/:id" element={<AddTicketComponent />} />
-              <Route path="/tickets-list" element={<Tickets />} />
-              <Route path="/test" element={<AddingMy />} />
-            </Route>
-
-            {/* Inventory */}
-            <Route element={<CheckAuth allowedRoles={[1, 2, 3, 4]} />}>
-              <Route path="/inventory" element={<InventoryMain />} />
-              <Route path="/AddInventory" element={<AddInventory />} />
-              <Route path="/UpdateInventory/:id" element={<AddInventory />} />
-              <Route path="/AddSimcards" element={<AddSimcards />} />
-              <Route path="/AddSimcards/:id" element={<AddSimcards />} />
-              <Route path="/simcards" element={<Simcards />} />
-            </Route>
-=======
-          <Route element={<PersistLogin/>}>
-              <Route element={<SidebarLayout />}>
-          
-                {/* Home */}
-                <Route element={<CheckAuth allowedRoles={[1,2,3,4]}/>}>
+          <Route element={<PersistLogin />}>
+            <Route element={<SidebarLayout />}>
+              {/* Home */}
+              <Route element={<CheckAuth allowedRoles={[1, 2, 3, 4]} />}>
                 <Route path="/home" element={<Home />} />
-                </Route>
+              </Route>
 
-                {/* User Management - accessed by only Admin and Super_User */}
-                <Route element={<CheckAuth allowedRoles={[3,4]}/>}>
-        
+              {/* User Management - accessed by only Admin and Super_User */}
+              <Route element={<CheckAuth allowedRoles={[3, 4]} />}>
                 <Route path="/list-user" element={<Users />} />
                 <Route path="/edit-user/:id" element={<RegisterComponent />} />
-                </Route>
+              </Route>
 
-                {/* Facilities */}
-                <Route element={<CheckAuth allowedRoles={[1,2,3,4]}/>}>
+              {/* Facilities */}
+              <Route element={<CheckAuth allowedRoles={[1, 2, 3, 4]} />}>
                 <Route path="/facilities" element={<Facilities />} />
-                </Route>
+              </Route>
 
-                {/* Tickets */}
-                <Route element={<CheckAuth allowedRoles={[1,2,3,4]}/>}>
+              {/* Tickets */}
+              <Route element={<CheckAuth allowedRoles={[1, 2, 3, 4]} />}>
                 <Route path="/tickets" element={<TicketsDashboard />} />
                 <Route path="/addticket" element={<AddTicketComponent />} />
-                <Route path="/edit-ticket/:id" element={<AddTicketComponent />} />
+                <Route
+                  path="/edit-ticket/:id"
+                  element={<AddTicketComponent />}
+                />
                 <Route path="/tickets-list" element={<Tickets />} />
-              
-                </Route>
-                
-                {/* Inventory */}
-                <Route element={<CheckAuth allowedRoles={[1,2,3,4]}/>}>
-                <Route path="/inventory" element={<Inventory />} />
+              </Route>
+
+              {/* Inventory */}
+              <Route element={<CheckAuth allowedRoles={[1, 2, 3, 4]} />}>
+                <Route path="/inventory" element={<InventoryMain />} />
                 <Route path="/AddInventory" element={<AddInventory />} />
                 <Route path="/UpdateInventory/:id" element={<AddInventory />} />
                 <Route path="/AddSimcards" element={<AddSimcards />} />
                 <Route path="/AddSimcards/:id" element={<AddSimcards />} />
                 <Route path="/simcards" element={<Simcards />} />
-                </Route>
-              
               </Route>
-                
->>>>>>> b992e2f3f2b1821702f770c340815a16be843acd
+            </Route>
           </Route>
           <Route path="/add-user" element={<RegisterComponent />} />
           <Route path="/" element={<Login />} />
           <Route path="/unauthorized" element={<Unauthorized />} />
-<<<<<<< HEAD
           <Route path="/sim" element={<Simcards />} />
-=======
-          <Route path="/test" element={<AddingMy />} />
->>>>>>> b992e2f3f2b1821702f770c340815a16be843acd
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </>
