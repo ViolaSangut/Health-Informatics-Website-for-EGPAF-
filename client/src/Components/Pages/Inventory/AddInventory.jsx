26052715768ad054da33d8cabdb11782b507f81d
NabWW <63785037+nabww@users.noreply.github.com>
2022-07-06 10:12:57 +0300

    input validation for tablet inventory

--- client/src/Components/Pages/Inventory/AddInventory.jsx
+++ client/src/Components/Pages/Inventory/AddInventory.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { toast  } from 'react-toastify';
 import './AddInventory.css'
@@ -12,37 +12,112 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const EMPTY_REGEX = /^(?!\s*$).+/;
+const ASSET_REGEX = /^[A-Za-z0-9 ]+$/
+const SERIAL_REGEX = /^[0-9]+$/
+const PIN_REGEX = /^[0-9]{4}$/;
+const EMAIL_REGEX = /^\S+@\S+\.\S+$/;
+const PWD_REGEX = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,24}$/;
 
 const AddInventory = ()=> {
+const nameRef = useRef();
 
 
+const[AssetNumber, setAssetNumber]= useState("")
+const [validNumber, setValidNumber]= useState(false);
+const [numberFocus, setNumberFocus]= useState(false);
 
-const [validField, setValidField] = useState("")
-    
 
-const[AssetNumber, setAssetNumber]= useState("") 
-const[serialNumber, setSerialNumber]= useState("") 
-const[AssetStatus, setAssetStatus]= useState("") 
-const[AssetName, setAssetName]= useState("") 
+const[serialNumber, setSerialNumber]= useState("")
+const [validSerial, setValidSerial]= useState(false);
+const [serialFocus, setSerialFocus]= useState(false);
+
+
+const[AssetStatus, setAssetStatus]= useState("")
+const [validStatus, setValidStatus]= useState(false);
+const [statusFocus, setStatusFocus]= useState(false);
+
+
+const[AssetName, setAssetName]= useState("")
+const [validName, setValidName]= useState(false);
+const [nameFocus, setNameFocus]= useState(false);
+
+
 const[facility, setFacility]= useState("")
-const[Email, setEmail]= useState("")  
+const [validFacility, setValidFacility]= useState(false);
+const [facilityFocus, setFacilityFocus]= useState(false);
+
+
+const[Email, setEmail]= useState("") 
+const [validEmail, setValidEmail]= useState(false);
+const [emailFocus, setEmailFocus]= useState(false);
+
+
 const[Passcode, setPassCode]= useState("")
+const [validPasscode, setValidPasscode]= useState(false);
+const [passcodeFocus, setPasscodeFocus]= useState(false);
+
 const[EmailPassword, setEmailPassword]= useState("")
+const [validPassword, setValidPassword]= useState(false);
+const [passwordFocus, setPasswordFocus]= useState(false);
+
+
 const {id} =useParams(); 
 const navigate = useNavigate();
 
 const [items, setItems] = useState([])
 const item = items.find(item => (item.id).toString() === id);
+
+//definition of private axios
 const private_axios = usePrivateAxios();
 
+//checking input against regex on entry
+useEffect(() => {
+    const result = SERIAL_REGEX.test(serialNumber);
+    // console.log(result);
+    setValidSerial(result);
+  }, [serialNumber]);
 
+  useEffect(() => {
+    const result = ASSET_REGEX.test(AssetNumber);
+    // console.log(result);
+    setValidNumber(result);
+  }, [AssetNumber]);
+
+  useEffect(() => {
+    const result = PIN_REGEX.test(Passcode);
+    // console.log(result);
+    setValidPasscode(result);
+  }, [Passcode]);
+  
+  useEffect(() => {
+    const result = EMAIL_REGEX.test(Email);
+    // console.log(result);
+    setValidEmail(result);
+  }, [Email]);
+
+  useEffect(() => {
+    const result = PWD_REGEX.test(EmailPassword);
+    // console.log(result);
+    setValidPassword(result);
+  }, [EmailPassword]);
+
+  useEffect(() => {
+    const result = ASSET_REGEX.test(AssetName);
+    // console.log(result);
+    setValidName(result);
+  }, [AssetName]);
 
 useEffect(() => {
-    const result = EMPTY_REGEX.test(validField)
-    console.log(result); 
-  setValidField(result);
-},[validField]);
+    const result = ASSET_REGEX.test(facility);
+    // console.log(result);
+    setValidFacility(result);
+  }, [facility]);
+
+  useEffect(() => {
+    const result = ASSET_REGEX.test(AssetStatus);
+    // console.log(result);
+    setValidStatus(result);
+  }, [AssetStatus]);
 
 
  useEffect(() => {
@@ -136,12 +211,7 @@ useEffect(() => {
   const onClickBack =() =>{
     navigate("/inventory")
   }
-    
-
-
-
-
-
+  
 
     return  (
         <div align ="middle">
@@ -151,11 +221,11 @@ useEffect(() => {
         <form className= "">
             <h1>Device Details</h1>
             <label>
-                <span className={validField ? "hide" : "invalid"}>
-                <FontAwesomeIcon icon={faTimes} />
+                <span className={validName ? "valid" : "hide"}>
+                <FontAwesomeIcon icon={faCheck} />
               </span>
-              <span className={!validField ? "valid" : "hide"}>
-                        <FontAwesomeIcon icon={faCheck} />
+              <span className={validName || !AssetName ? "hide" : "invalid"}>
+                        <FontAwesomeIcon icon={faTimes} />
                     </span>
                 Brand Name
             </label>
@@ -165,16 +235,29 @@ useEffect(() => {
             placeholder='Asset Name' 
             type='text'  
             required
-                aria-invalid={validField ? "false" : "true"}
-                aria-describedby="validid"
+                aria-invalid={validName ? "false" : "true"}
+                aria-describedby="nameid"
+                onFocus={() => setNameFocus(true)}
+              onBlur={() => setNameFocus(false)}
+            />
+            <p
+              id="nameid"
+              className={
+                nameFocus && AssetName && !validName
+                  ? "instructions"
+                  : "offscreen"
+              }
             >
-            </input>
+                <FontAwesomeIcon icon={faInfoCircle} />
+              <br />
+              Brand Name cannot contain numbers!
+            </p>
             <label>
-                <span className={validField ? "hide" : "invalid"}>
-                <FontAwesomeIcon icon={faTimes} />
+                <span className={validNumber ? "valid" : "hide"}>
+                <FontAwesomeIcon icon={faCheck} />
               </span>
-              <span className={!validField ? "valid" : "hide"}>
-                        <FontAwesomeIcon icon={faCheck} />
+              <span className={validNumber || !AssetNumber ? "hide" : "invalid"}>
+                        <FontAwesomeIcon icon={faTimes} />
                     </span>
                 Asset Number
             </label>
@@ -184,16 +267,30 @@ useEffect(() => {
             placeholder='Asset Number' 
             type='text'
             required
-                aria-invalid={validField ? "false" : "true"}
+                aria-invalid={validNumber ? "false" : "true"}
                 aria-describedby="validid"
+            onFocus={() => setNumberFocus(true)}
+              onBlur={() => setNumberFocus(false)}
             >
             </input>
+            <p
+              id="validid"
+              className={
+                numberFocus && AssetNumber && !validNumber
+                  ? "instructions"
+                  : "offscreen"
+              }
+            >
+                <FontAwesomeIcon icon={faInfoCircle} />
+              <br />
+              Must have be a valid Asset Number containing numbers only!
+            </p>
             <label>
-                <span className={validField ? "hide" : "invalid"}>
-                <FontAwesomeIcon icon={faTimes} />
+                <span className={validSerial ? "valid" : "hide"}>
+                <FontAwesomeIcon icon={faCheck} />
               </span>
-              <span className={!validField ? "valid" : "hide"}>
-                        <FontAwesomeIcon icon={faCheck} />
+              <span className={validSerial || !serialNumber ? "hide" : "invalid"}>
+                        <FontAwesomeIcon icon={faTimes} />
                     </span>
                 Serial Number
             </label>
@@ -204,27 +301,43 @@ useEffect(() => {
             placeholder='Asset Number' 
             type='text'
             required
-                aria-invalid={validField ? "false" : "true"}
-                aria-describedby="validid"
+                aria-invalid={validSerial ? "false" : "true"}
+                aria-describedby="serialid"
+                onFocus={() => setSerialFocus(true)}
+              onBlur={() => setSerialFocus(false)}
             >
                 
             </input>
+            <p
+              id="serialid"
+              className={
+                serialFocus && serialNumber && !validSerial
+                  ? "instructions"
+                  : "offscreen"
+              }
+            >
+                <FontAwesomeIcon icon={faInfoCircle} />
+              <br />
+              Must have be a valid serial Number containing numbers only!
+            </p>
             <label>
-                <span className={validField ? "hide" : "invalid"}>
-                <FontAwesomeIcon icon={faTimes} />
+                <span className={validStatus ? "valid" : "hide"}>
+                <FontAwesomeIcon icon={faCheck} />
               </span>
-              <span className={!validField ? "valid" : "hide"}>
-                        <FontAwesomeIcon icon={faCheck} />
+              <span className={validStatus || !AssetStatus ? "hide" : "invalid"}>
+                        <FontAwesomeIcon icon={faTimes} />
                     </span>
                 Status
             </label>
-            
+           
             <select 
             value={AssetStatus} 
             onChange ={(e) => setAssetStatus(e.target.value)}
             required
-                aria-invalid={validField ? "false" : "true"}
-                aria-describedby="validid"
+                aria-invalid={validStatus ? "false" : "true"}
+                aria-describedby="statusid"
+                onFocus={() => setStatusFocus(true)}
+              onBlur={() => setStatusFocus(false)}
             >
                   <option selected disabled="true">--Select Device Status--</option>
                 {
@@ -232,13 +345,24 @@ useEffect(() => {
                 }
                  
             </select>
-            
+             <p
+              id="statusid"
+              className={
+                statusFocus && AssetStatus && !validStatus
+                  ? "instructions"
+                  : "offscreen"
+              }
+            >
+                <FontAwesomeIcon icon={faInfoCircle} />
+              <br />
+              Please select the current device status!
+            </p>
             <label>
-                <span className={validField ? "hide" : "invalid"}>
-                <FontAwesomeIcon icon={faTimes} />
+                <span className={validFacility ? "valid" : "hide"}>
+                <FontAwesomeIcon icon={faCheck} />
               </span>
-              <span className={!validField ? "valid" : "hide"}>
-                        <FontAwesomeIcon icon={faCheck} />
+              <span className={validFacility || !facility ? "hide" : "invalid"}>
+                        <FontAwesomeIcon icon={faTimes} />
                     </span>
                 Facility Assigned
             </label>
@@ -247,21 +371,34 @@ useEffect(() => {
              value={facility} 
              onChange ={(e) => setFacility(e.target.value)}
              required
-                aria-invalid={validField ? "false" : "true"}
-                aria-describedby="validid"
+                aria-invalid={validFacility ? "false" : "true"}
+                aria-describedby="facilityid"
+                onFocus={() => setFacilityFocus(true)}
+              onBlur={() => setFacilityFocus(false)}
              >
                   <option selected disabled="true">--Select Facility Assigned--</option>
                 {
                      Facilities.Facilitynames.map((result)=>(<option text={result.no}>{result.facility}</option>))
                 }
             </select>
-            
+            <p
+              id="facilityid"
+              className={
+                facilityFocus && facility && !validFacility
+                  ? "instructions"
+                  : "offscreen"
+              }
+            >
+                <FontAwesomeIcon icon={faInfoCircle} />
+              <br />
+              Please select the facility assigned to this device!
+            </p>
              <label>
-                 <span className={validField ? "hide" : "invalid"}>
-                <FontAwesomeIcon icon={faTimes} />
+                 <span className={validPasscode ? "valid" : "hide"}>
+                <FontAwesomeIcon icon={faCheck} />
               </span>
-              <span className={!validField ? "valid" : "hide"}>
-                        <FontAwesomeIcon icon={faCheck} />
+              <span className={validPasscode || !Passcode ? "hide" : "invalid"}>
+                        <FontAwesomeIcon icon={faTimes} />
                     </span>
                 Passcode
             </label>
@@ -271,16 +408,30 @@ useEffect(() => {
             placeholder='Passcode' 
             type='text'
             required
-                aria-invalid={validField ? "false" : "true"}
-                aria-describedby="validid"
+                aria-invalid={validPasscode ? "false" : "true"}
+                aria-describedby="passid"
+                onFocus={() => setPasscodeFocus(true)}
+              onBlur={() => setPasscodeFocus(false)}
             >
             </input>
+            <p
+              id="passid"
+              className={
+                passcodeFocus && Passcode && !validPasscode
+                  ? "instructions"
+                  : "offscreen"
+              }
+            >
+                <FontAwesomeIcon icon={faInfoCircle} />
+              <br />
+              Passcode can only be 4 digits!
+            </p>
             <label>
-                 <span className={validField ? "hide" : "invalid"}>
-                <FontAwesomeIcon icon={faTimes} />
+                 <span className={validEmail ? "valid" : "hide"}>
+                <FontAwesomeIcon icon={faCheck} />
               </span>
-              <span className={!validField ? "valid" : "hide"}>
-                        <FontAwesomeIcon icon={faCheck} />
+              <span className={validEmail || !Email ? "hide" : "invalid"}>
+                        <FontAwesomeIcon icon={faTimes} />
                     </span>
                 Email
             </label>
@@ -290,31 +441,64 @@ useEffect(() => {
             placeholder='Email' 
             type='text'
             required
-                aria-invalid={validField ? "false" : "true"}
+                aria-invalid={validEmail ? "false" : "true"}
                 aria-describedby="validid"
+            onFocus={() => setEmailFocus(true)}
+              onBlur={() => setEmailFocus(false)}
             >
             </input>
+            <p
+              id="passid"
+              className={
+                emailFocus && Email && !validEmail
+                  ? "instructions"
+                  : "offscreen"
+              }
+            >
+                <FontAwesomeIcon icon={faInfoCircle} />
+              <br />
+              Has to be a valid Email!
+            </p>
             <label>
-                <span className={!validField ? "valid" : "hide"}>
-                        <FontAwesomeIcon icon={faCheck} />
-                    </span>
-                 <span className={validField ? "hide" : "invalid"}>
-                <FontAwesomeIcon icon={faTimes} />
+                <span className={validPassword ? "valid" : "hide"}>
+                <FontAwesomeIcon icon={faCheck} />
               </span>
+              <span className={validPassword || !EmailPassword ? "hide" : "invalid"}>
+                        <FontAwesomeIcon icon={faTimes} />
+                    </span>
                 Email Password
             </label>
             <input 
             value={EmailPassword} 
             onChange ={(e) => setEmailPassword(e.target.value)} 
             placeholder='Email Password' 
-            type='text'>
+            type='text'
+            required
+                aria-invalid={validEmail ? "false" : "true"}
+                aria-describedby="passwordid"
+            onFocus={() => setPasswordFocus(true)}
+              onBlur={() => setPasswordFocus(false)}
+            >
             </input>
-
+              <p
+              id="passwordid"
+              className={
+                passwordFocus && EmailPassword && !validPassword
+                  ? "instructions"
+                  : "offscreen"
+              }
+            >
+                <FontAwesomeIcon icon={faInfoCircle} />
+              <br />
+              Password has to have, one special character, uppercase and 
+              lowercase letters and a number. Special characters are also allowed.
+            </p>
             <label>
              
             </label>
-            <button onClick={handleSubmit} align="middle" disabled={
-                    !validField ? true: false}>
+            <button onClick={handleSubmit} align="middle" 
+            disabled={(!validFacility|| !validEmail || !validPassword || !validPasscode || !validSerial || !validStatus || !validName || !validNumber) ? true : false}
+            >
                 Submit
             </button>
 
